09f0b97a2a26e254765b54327c82a7963a515924
PauSGracia <pau.sole@gracia.lasalle.cat>
2025-12-01 17:38:37 +0100

    feat: player choose, win/lose

--- main.ts
+++ main.ts
@@ -1,5 +1,27 @@
+let player_hand = 0
 let cpu_hand = 0
-input.onGesture(Gesture.Shake, function () {
+input.onButtonPressed(Button.A, function () {
+    player_hand += 1
+    if (player_hand > 3) {
+        player_hand = 1
+    }
+    if (player_hand == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (player_hand == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("CPU")
     cpu_hand = randint(1, 3)
     if (cpu_hand == 1) {
         basic.showIcon(IconNames.SmallSquare)
@@ -11,4 +33,35 @@ input.onGesture(Gesture.Shake, function () {
         basic.showIcon(IconNames.Scissors)
         music.play(music.createSoundExpression(WaveShape.Square, 1522, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    if (player_hand == cpu_hand) {
+        basic.showString("DRAW")
+    } else {
+        if (player_hand == 1) {
+            if (cpu_hand == 2) {
+                basic.showString("LOSE")
+            } else {
+                basic.showString("WIN")
+            }
+        } else if (player_hand == 2) {
+            if (cpu_hand == 3) {
+                basic.showString("LOSE")
+            } else {
+                basic.showString("WIN")
+            }
+        } else {
+            if (cpu_hand == 1) {
+                basic.showString("LOSE")
+            } else {
+                basic.showString("WIN")
+            }
+        }
+    }
+    player_hand = 0
 })
