893085a99a2d0ffd033d6bf304062ab64b44af52
PauSGracia <pau.sole@gracia.lasalle.cat>
2025-12-01 16:24:15 +0100

    feat: cpu

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
+let cpu_hand = 0
+input.onGesture(Gesture.Shake, function () {
+    cpu_hand = randint(1, 3)
+    if (cpu_hand == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+    } else if (cpu_hand == 2) {
+        basic.showIcon(IconNames.Square)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+        music.play(music.createSoundExpression(WaveShape.Square, 1522, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    }
 })
